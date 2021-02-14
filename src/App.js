ca3dfc958527aa54a824a6023b0decc846960b62
Lucas Legrand <legrand.lucas0@gmail.com>
2021-02-14 18:51:15 +0100

    React login starter file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from '../src/Pages/Dashboard';
+import Login from './Components/Login';
+import useToken from './Components/useToken';
 
 function App() {
+
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
   return (
     <div className="wrapper">
       <h1>Application</h1>
